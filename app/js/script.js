56002bcd2885b49103632e24d3b9fe3e09e0803a
Fastest17 <kavaletsvasia@gmail.com>
2019-05-17 00:34:01 +0300

    download

---
+++ app/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    $(".nav_menu").animated("fadeInDown");
+    $(".top_img").animated("fadeInRight");
+
+    $(".video_container").animated("zoomIn");
+    $(".about_content").animated("zoomIn");
+
+    $(".radio_container").animated("fadeInLeft");
+
+    $(".subscr_left").animated("fadeInLeft");
+    $(".subscr_right").animated("fadeInRight");
+
+    $(".photo_item").animated("flipInX");
+
+
+    $(".popup").magnificPopup({type: "image"});
+    
+});
+
